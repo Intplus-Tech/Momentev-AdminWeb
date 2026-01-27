e96bf3164195f719071d9451090ba7b43055458b
cyberrspace <xhamooel95@gmail.com>
2026-01-27 19:27:29 +0100

    WIP: admin sidebar and categories setup

---
+++ app/(admin)/admin/categories/page.tsx
@@ -0,0 +1,20 @@
+import CategoryTable from "./_components/CategoryTable";
+
+export default function CategoryPage(){
+return(
+<div className="space-y-4">
+<CategoryTable/>
+
+
+    <div>
+      <button className="bg-primary">
+        Add Category
+      </button>
+    </div>
+
+</div>
+
+
+)
+
+}
